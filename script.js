e7e5ae52c975b78d146981d843f12de45805320f
nola360 <adaramolajr@gmail.com>
2020-01-01 21:51:38 -0500

    added html and js content primitive & reference types

---
+++ script.js
@@ -0,0 +1,44 @@
+// Primitive Types
+
+// String
+const name = 'John Doe';
+console.log(typeof name);
+
+// Number
+const age = 65;
+console.log(typeof age);
+
+// Boolean
+const webDeveloper = true;
+console.log(typeof webDeveloper);
+
+// Null
+const car = null;
+console.log(typeof car);
+
+// Undefined
+let animal;
+console.log(typeof animal);
+
+// Symbol 
+const sym = Symbol();
+console.log(typeof sym);
+
+
+
+// Reference Types
+
+// Arrays
+const hobbies = ['writing', 'guitar', 'web development'];
+console.log(typeof hobbies);
+
+// Object Literal
+
+const address = {
+  city: 'Providence',
+  State: "Rhode Island"
+}
+console.log(typeof address);
+
+const today = new Date();
+console.log(typeof today);
