027bdd7eeeefbc0904e8bef77bf334930421efec
nola360 <adaramolajr@gmail.com>
2020-01-01 22:13:31 -0500

    logged type conversion and type coersion

--- script.js
+++ script.js
@@ -1,44 +1,62 @@
-// Primitive Types
+// TYPE CONVERSION
 
-// String
-const name = 'John Doe';
-console.log(typeof name);
+let val;
 
-// Number
-const age = 65;
-console.log(typeof age);
+// Convert number to string
+val = String(555);
+// expression below will overright the expression above
+val = String(4 + 4);
 
-// Boolean
-const webDeveloper = true;
-console.log(typeof webDeveloper);
+// Boolean to string
+val = String(true);
 
-// Null
-const car = null;
-console.log(typeof car);
+// Date to a String
+val = String(new Date());
 
-// Undefined
-let animal;
-console.log(typeof animal);
+// Array to String
+val = String([1, 2, 3, 4, 5, 6, 7, 8]);
 
-// Symbol 
-const sym = Symbol();
-console.log(typeof sym);
+//convert number toString() same as line 3
+val = (5).toString();
+val = (true).toString();
 
 
+// String to Number
+val = Number('5');
 
-// Reference Types
+// Boolean to numebr
 
-// Arrays
-const hobbies = ['writing', 'guitar', 'web development'];
-console.log(typeof hobbies);
+// logs 1
+val = Number(true);
+// Logs 0
+val = Number(false);
+// Logs 0
+val = Number(null);
 
-// Object Literal
+// Logs NaN
+val = Number('Hello');
+val = Number([1, 2, 3]);
 
-const address = {
-  city: 'Providence',
-  State: "Rhode Island"
-}
-console.log(typeof address);
 
-const today = new Date();
-console.log(typeof today);
+// Parse Int
+// Will only log 100 as an integer 
+val = parseInt('100.30');
+
+// ParseFloat will log 100.3
+val = parseFloat('100.30');
+
+
+// Output
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed(2));
+
+// TYPE COERSION - will not acknowledge the data type
+const val1 = String(5);
+const val2 = 6;
+// Converts expression to number 56
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
